79dbe21f30883d42454ca1703d7e08c2c81a60b2
Divine Agbasimelo <divinegodwin172@gmail.com>
2024-06-05 16:20:37 +0100

    added region

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 function App() {
   const date = new Date();
   const hours = date.getHours();
@@ -14,28 +14,36 @@ function App() {
     fullTime = `${hours}:${minutes}${format[1]}`;
   }
   const [userInput, setUserInput] = useState("");
+  const [continent, setContinent] = useState("");
+  const [error, setError] = useState('')
+  const [country, setCountry] = useState('')
 
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
   };
 
-  const myKey = "AIzaSyCvfHXY8kwClaF4GLYsjUSw-fzWRamaWo";
+  const handleSelect = (e) =>{
+    setContinent(e.target.value)
+  }
 
-  useEffect(() => {
-    fetch(
-      `https://maps.googleapis.com/maps/api/timezone/json?location=${userInput}&timestamp=1331161200&key=${myKey}`
-    )
-      .then(function (response) {
-        response.json();
-      })
-      .then(function (data) {
-        console.log(data);
-      })
-      .catch((error) => {
-        console.warn(error);
-      });
-      
-  }, []);
+  const fetchData = () =>{     
+        
+    if(continent && userInput){
+
+    fetch(`http://worldtimeapi.org/api/timezone/${continent}/${userInput}`)
+
+     .then(response => response.json())
+     .then(data=>{
+      console.log(data)
+      setCountry()
+     })
+     .catch(error =>console.log(error))
+   }
+   else{
+    console.log('please select a valid location')
+    setError('please input a valid loacation')
+   }
+  }
 
   return (
     <>
@@ -53,7 +61,25 @@ function App() {
           </div>
         </div>
 
-        <div className="flex justify-center ml-3 mt-[2rem] pt-[10px] h-[60px] bg-slate-200 w-[360px] rounded-full">
+
+        <div className="mt-4 ml-4">
+        <label>select region:</label><br />
+        <select className="text-blue-500"
+        onChange={handleSelect}
+        >
+          <option value='Nort America'>North America</option>
+          <option value='South America'>South America</option>
+          <option value='Africa'>Africa</option>
+          <option value='Asia'>Asia</option>
+          <option value='Europe'>Europe</option>
+          <option value='artactica'>Artactica</option>
+        </select>
+</div>
+
+
+        <div className="flex justify-center ml-3 mt-[1rem] pt-[10px] h-[60px] bg-slate-200 w-[360px] rounded-full">
+
+            <div className="text-[red]">{error}</div>
           <input
             className="w-[280px] h-[40px] p-4 outline-orange-500 rounded-full"
             onChange={handleInputChange}
@@ -61,7 +87,8 @@ function App() {
           ></input>
 
           <svg
-          className="w-[40px] pl-1 pb-2"
+          onClick={fetchData}
+          className="w-[40px] pl-1 pb-2 text-blue-500"
             data-slot="icon"
             fill="none"
             strokeWidth="1.5"
@@ -76,11 +103,17 @@ function App() {
               d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
             ></path>
           </svg>
+          </div>
+
         </div>
 
-        <div className="bg-slate-200 w-[90%] h-[200px] rounded-lg ml-4 mt-8"></div>
+        <div className="bg-slate-200 w-[90%] h-[200px] rounded-lg ml-4 mt-8">
+
+            <header>{}</header>
+
+        </div>
         
-      </div>
+      
     </>
   );
 }
